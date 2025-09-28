4294cf7525579e4a05dabbef24b811138635e28f
Luke <lukefanelli2@gmail.com>
2025-09-27 20:11:15 -0400

    added some stand functionality

--- cardGame.js
+++ cardGame.js
@@ -13,28 +13,31 @@ const cards = {
     12: "Cards/12card.png",
     13: "Cards/13card.png",
 };
-const ai = document.getElementsByClassName("output");
-const player = document.getElementsByClassName("poutput");
+const ai = document.getElementsByClassName("output"); //ai hand
+const player = document.getElementsByClassName("poutput"); //player hand
 
 const aiHandVal = document.getElementById("jtext"); //output msg for ai hand amount
 const pHandVal = document.getElementById("ptext"); //output msg for p hand amount
 
 const start = document.getElementById("start"); //start btn
 const hitBtn = document.getElementById("hit"); //hit btn
+const standBtn = document.getElementById("stand"); //stand btn
 
+let pHand = []; //arr to store key's for value calculation
+let aiHand = []; //arr to store key's for value calculation
 
-let pHand = [];
-let aiHand = [];
-
+//on start click generate cards and enable hit & stand
 function getAiCards(){
     start.addEventListener("click", function(){
         switchImage(ai, aiHand, aiHandVal);
         switchImage(player, pHand, pHandVal);
         this.disabled = true;
         hitBtn.disabled = false;
+        standBtn.disabled = false;
     })
 }
 
+//function to get random card and assign it to selected handArr
 function switchImage(elements, handArr, handArrDisplay) {
     for (let i = 0; i < elements.length; i++){
         const card = getRandValue();
@@ -44,6 +47,7 @@ function switchImage(elements, handArr, handArrDisplay) {
     getValue(handArrDisplay, handArr);
 }
 
+//returns the keys and value of each card (value being the file path & key being the key of said value)
 function getRandValue() {
     const values = Object.values(cards); // returns values of obj keys
     const keys = Object.keys(cards); //returns keys of values
@@ -55,6 +59,7 @@ function getRandValue() {
     return output;
 }
 
+//adds random card to player hand and assigns class "poutput" to rand card generated
 function hit(){
     hitBtn.disabled = true;
     const img = document.getElementsByClassName("createimg");
@@ -73,18 +78,29 @@ function hit(){
     })
 }
 
+//get total value of hand and assign total to msg
 function getValue(msg, handArr){
     const total = handArr.reduce((acc, curr) => acc + curr, 0);
     msg.innerHTML = `Hand's Value: ${total}`;
 }
 
+//if card is a face value card (king, jack, queen) set value to 10
 function getCardFaceValue(cardNum){
     if (cardNum >= 11) return 10;
     return cardNum;
 }
 
+//does shit
+function stand(){
+    standBtn.disabled = true;
+    standBtn.addEventListener("click", function(){
+        standBtn.disabled = true;
+        hitBtn.disabled = true;
+    });
+}
 
+ 
 
-
+stand();
 hit();
 getAiCards();
